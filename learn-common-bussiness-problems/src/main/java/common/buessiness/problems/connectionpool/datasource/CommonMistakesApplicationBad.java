cce8a49a083d77e068f4573687d64a37f9220ba5
mingduo <weizc@asiainfo.com>
2020-08-06 22:07:44 +0800

    fix

--- learn-common-bussiness-problems/src/main/java/common/buessiness/problems/connectionpool/datasource/CommonMistakesApplicationBad.java
+++ learn-common-bussiness-problems/src/main/java/common/buessiness/problems/connectionpool/datasource/CommonMistakesApplicationBad.java
@@ -1,14 +1,18 @@
 package common.buessiness.problems.connectionpool.datasource;
 
+import common.buessiness.problems.utils.SystemPropertySourceUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 
-@PropertySource("classpath:common/buessiness/problems/connectionpool/datasource/bad.properties")
 @SpringBootApplication
 public class CommonMistakesApplicationBad {
 
     public static void main(String[] args) {
+        ClassPathResource classPathResource = new ClassPathResource("common/buessiness/problems/connectionpool/datasource/bad.properties");
+        SystemPropertySourceUtils.loadProperties(classPathResource);
+
         SpringApplication.run(CommonMistakesApplicationBad.class, args);
     }
 }
